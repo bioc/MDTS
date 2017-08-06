32b89e99f3380999527d828b505544d59de52b68
Jack Fu <jmfu@jhsph.edu>
2017-08-06 11:32:55 -0400

    package info

---
+++ R/DnMD-package.R
@@ -0,0 +1,12 @@
+#' The DnMD package for Detection of Denovo Deletions from Targeted Sequencing Data Using Minimum-Distance
+#'
+#' @name DnMD-package
+#' @aliases DnMD
+#' @docType package
+#' @import GenomicRanges
+#' @import IRanges
+#' @import S4Vectors
+#' @import Repitools
+#' @import stringr
+#' @import methods
+NULL
