b5541117aae145881eaecf2ca9a2187c6e34268c
Jack Fu <jmfu@jhsph.edu>
2017-08-06 11:19:23 -0400

    bin calculation

---
+++ R/calcBinHelpers.R
@@ -0,0 +1,59 @@
+processChr = function(chr, proto_info, covs, rl, med){
+	print(paste0("Selecting Proto-regions in Chr ", chr)); flush.console()
+	proto_region = red[[chr]]
+	proto_gr = reduce(GRanges(seqnames=chr, IRanges(start=proto_region, end=proto_region)))
+	proto_gr_covs_rle = lapply(covs, function(x) x[proto_gr])
+	proto_gr_covs = lapply(proto_gr_covs_rle, function(x) lapply(x, sum))
+	proto_gr_covs_mat = apply(do.call(rbind, proto_gr_covs), 2, as.numeric)
+	proto_gr_covs_mat_normed = t(t(proto_gr_covs_mat)/rl)
+	proto_gr_covs_mat_med = apply(proto_gr_covs_mat_normed, 2, median)
+		proto_gr$reads = proto_gr_covs_mat_med
+	proto_gr_select = proto_gr[proto_gr$reads>=med]
+
+	if(length(proto_gr_select)>0){
+		print(paste0("Segmenting Chr ", chr, " Proto-regions")); flush.console()
+		pb = txtProgressBar(min = 0, max = length(proto_gr_select), style = 3)
+		chr_out = NULL
+		for(i in 1:length(proto_gr_select)){
+			setTxtProgressBar(pb, i)
+			chr_out = c(chr_out, divideSegs(proto_gr_select[i], covs, rl, med))
+		}
+		print(); flush.console()
+		chr_out = do.call('c', chr_out)
+	}else{
+		return(NULL)
+	}
+	return(chr_out)
+}
+
+extractCounts = function(cov_list){
+	return(suppressWarnings((as.numeric(unlist(cov_list)))))
+}
+
+divideSegs = function(seg, covs, rl, med){
+	output = NULL
+	i = 1
+	count = 1
+	num_segs = floor(seg$reads/med)
+	cov_sub = lapply(covs, function(x) x[seg])
+	cov_sub_mat = do.call(rbind, lapply(cov_sub, extractCounts))
+	while(count<num_segs){
+		cov_sub_cs = t(apply(cov_sub_mat[,i:dim(cov_sub_mat)[2]], 1, cumsum))
+		cov_sub_cs_normed = cov_sub_cs/rl
+		medz = apply(cov_sub_cs_normed, 2, median)
+		cut = which(medz>=med)[1]
+		if(!is.na(cut)){
+			output = rbind(output, c(cut, medz[cut]))
+		}
+		count = count+1; i = sum(output[,1])+1
+	}
+	cov_sub_cs = t(apply(cov_sub_mat[,i:dim(cov_sub_mat)[2]], 1, cumsum))
+	cov_sub_cs_normed = cov_sub_cs/rl
+	medz = apply(cov_sub_cs_normed, 2, median)
+	output = rbind(output, c(width(seg)-sum(output[,1]), max(medz)))
+	cs = cumsum(output[,1])
+	coords = cbind(c(0, cs[-length(cs)]), cs-1)
+	gr_out = GRanges(seqnames=seqnames(seg), IRanges(start(seg)+coords[,1], start(seg)+coords[,2]))
+		gr_out$median_count = output[,2]
+	return(gr_out)
+}
---
+++ R/calcBins.R
@@ -0,0 +1,35 @@
+#' Sample driven calculation of bins for counting
+#'
+#' This function will randomly select a sample of bam files to calculate a range of bins to calculate read-depth on
+#' @pData A table in the format of the output of pData()
+#' @n The number of subsamples to use
+#' @med The median number of reads across sub-samples to reach before creating a new bin
+#' @seed Sets the seed so results are reproducible. Defaults to 1337
+#' @keywords calcBins
+#' @export
+#' @examples
+#' calcBins() 
+calcBins <- function(pData, n, rl, med, seed=1337){
+	set.seed(seed)
+	pD_sub = pD[sample(1:dim(pD)[1], n, replace=F),]
+	
+	print("Reading coverage information of subsamples"); flush.console()
+	covs = list()
+	for(i in 1:length(pD_sub$bam_path)){
+		GR=BAM2GRanges(pD_sub$bam_path[i])
+		reads = coverage(GR)
+		reads = reads[names(reads) %in% 1:22]
+		covs[[i]] = reads
+	}
+
+	print("Calculating Proto-regions"); flush.console()
+	track = lapply(covs, function(x) which(x>=min))
+	red = Reduce(intersect, track)
+
+	bins = NULL
+	for(chromosome in 1:22){
+		bins = c(bins, processChr(chromosome, red, covs, rl, med))
+	}
+	bins_out = suppressWarnings(do.call('c', bins))
+}
+
