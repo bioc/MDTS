67a3ac2583ea23095a1b27e312f24b2947b3909e
Jack Fu <jmfu@jhsph.edu>
2017-10-04 13:36:53 -0400

    calcCounts fix

--- R/calcCounts.R
+++ R/calcCounts.R
@@ -3,11 +3,13 @@
 #' This function will return a matrix object where ecah column is a sample, and each row is the count in a bin
 #' @param pData A table in the format of the output of pData()
 #' @param bins The set of bins determined by calcBins
+#' @param rl The read length of the experiment
 #' @keywords calcCounts
 #' @export
-calcCounts <- function(pData, bins){
+calcCounts <- function(pData, bins, rl){
 	cov_list = lapply(pData$bam_path, countHelper, bins)
 	cov_matrix = do.call(cbind, cov_list)
 	colnames(cov_matrix) = pData$subj_id
-	return(cov_matrix)
+	count_matrix = cov_matrix*width(bins)/rl
+	return(count_matrix)
 }
--- R/segmentMD.R
+++ R/segmentMD.R
@@ -8,7 +8,8 @@
 segmentMD = function(md, bins){
 	cna = CNA(genomdat=md, chrom=as.vector(seqnames(bins)),
 		maploc=start(bins), data.type="logratio", sampleid=colnames(md), presorted=T)
-	cbs = segment(cna, alpha=0.001, undo.splits="sdundo", undo.SD=4)
+	# cbs = segment(cna, alpha=0.001, undo.splits="sdundo", undo.SD=4)
+	cbs = segment(cna)
 	gr = GRanges(seqnames=cbs$output$chrom, 
 		IRanges(cbs$output$loc.start, cbs$output$loc.end), 
 		m=cbs$output$seg.mean, 
