d273b909dbcccc5a240010569f475118d1138ac5
Jack Fu <jmfu@jhsph.edu>
2017-10-05 08:15:00 -0400

    documentation update

--- R/segmentMD.R
+++ R/segmentMD.R
@@ -1,15 +1,16 @@
-#' This function calls CBS on a matrix of minimum distances to calculate copy number segments
+#' Circular Binary Segmentation on Minimum Distances
 #'
-#' This function will return a single GRanges object containing the copy number segments of all families in input
-#' @param md The minimum distance matrix produced by calcMD
-#' @param bins The set of bins determined by calcBins
+#' This function will return a GRanges object containing the copy number 
+#'	segments of all families in the input minimum distance matrix.
+#' @param md The minimum distance matrix produced by calcMD.
+#' @param bins The set of bins determined by calcBins.
 #' @keywords segmentMD
 #' @export
 segmentMD = function(md, bins){
 	cna = CNA(genomdat=md, chrom=as.vector(seqnames(bins)),
 		maploc=start(bins), data.type="logratio", sampleid=colnames(md), presorted=T)
-	# cbs = segment(cna, alpha=0.001, undo.splits="sdundo", undo.SD=4)
-	cbs = segment(cna)
+	cbs = segment(cna, alpha=0.001, undo.splits="sdundo", undo.SD=4)
+	# cbs = segment(cna)
 	gr = GRanges(seqnames=cbs$output$chrom, 
 		IRanges(cbs$output$loc.start, cbs$output$loc.end), 
 		m=cbs$output$seg.mean, 
