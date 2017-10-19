7e541bd5a6655e0a5b5ad0734ac9aeda148ead5e
Jack Fu <jmfu@jhsph.edu>
2017-10-19 10:53:07 -0400

    segmentMD binning change

--- R/segmentMD.R
+++ R/segmentMD.R
@@ -5,14 +5,18 @@
 #'	segment() from DNACopy with (alpha=0.001, undo.splits="sdundo", undo.SD=4)
 #' @param md The minimum distance matrix produced by calcMD.
 #' @param bins The set of bins determined by calcBins.
+#' @param alpha Controls the alpha option in calling cbs()
+#' @param undo.splits Controls the undo.splits option in calling cbs()
+#' @param undo.SD Controls the undo.SD option in calling cbs()
 #' @keywords segmentMD
 #' @export
-segmentMD = function(md, bins){
+segmentMD = function(md, bins, alpha='0.01', undo.splits='sdundo', undo.SD=4){
 	cna = CNA(genomdat=md, chrom=as.vector(seqnames(bins)),
 		maploc=start(bins), data.type="logratio", sampleid=colnames(md), presorted=T)
-	cbs = segment(cna, alpha=0.001, undo.splits="sdundo", undo.SD=4)
+	cbs = segment(cna, alpha=alpha, undo.splits=undo.splits, undo.SD=undo.SD)
+	segRows = cbs$segRows
 	gr = GRanges(seqnames=cbs$output$chrom, 
-		IRanges(cbs$output$loc.start, cbs$output$loc.end), 
+		IRanges(start(bins)[segRows[,1]], end(bins)[segRows[,2]]), 
 		m=cbs$output$seg.mean, 
 		famid=cbs$output$ID)
 	return(gr)
