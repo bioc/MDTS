a3329949e4aa5584c4630c68923d74ec755805d6
Jack Fu <jmfu@jhsph.edu>
2017-10-19 12:14:30 -0400

    example format update

--- R/calcMD.R
+++ R/calcMD.R
@@ -6,12 +6,12 @@
 #' @param bins The set of bins determined by calcBins().
 #' @keywords calcMD
 #' @examples 
-#' 		setwd(system.file('extdata', package='MDTS'))
-#'		load('bins.RData')
-#'		load('counts.RData')
-#'		load('pD.RData')
-#' 		mCounts = normalizeCounts(counts, bins)
-#'		md = calcMD(mCounts, bins, pD)
+#'	setwd(system.file('extdata', package='MDTS'))
+#'	load('bins.RData')
+#'	load('counts.RData')
+#'	load('pD.RData')
+#' 	mCounts = normalizeCounts(counts, bins)
+#'	md = calcMD(mCounts, bins, pD)
 #' @export
 calcMD = function(mCounts, bins, pData){
 	proband_ind = which(pData$father_id %in% pData$subj_id)
