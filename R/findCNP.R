24705a0255a395cb01d0120f6ae42c67c3e3687b
Jack Fu <jmfu@jhsph.edu>
2017-10-30 19:26:35 -0400

    inferred states for cnps

--- R/findCNP.R
+++ R/findCNP.R
@@ -14,5 +14,24 @@ findCNP = function(mCounts, bins, pData){
 	range = reduce(IRanges(candidate, candidate))
 	candidate_range = range[width(range)>1]
 	cnp = GRanges(seqnames(bins)[start(candidate_range)], IRanges(start(bins)[start(candidate_range)], end(bins)[end(candidate_range)]))
+
+	vals = NULL
+	for(i in 1:length(cnp)){
+		candidate = cnp[i]
+		ol = findOverlaps(candidate, bins)
+		mCountsCandidate = apply(mCounts[subjectHits(ol),], 2, mean)
+		s0 = sum(mCountsCandidate<=-1.5)
+		s1 = sum(mCountsCandidate>-1.5 & mCountsCandidate<=0.5)
+		s2 = sum(mCountsCandidate>0.5)
+
+		mCountsParents = mCountsCandidate[names(mCountsCandidate) %in% pD$subj_id[pD$mother_id==0]]
+		p0 = sum(mCountsParents<=-1.5)
+		p1 = sum(mCountsParents>-1.5 & mCountsParents<=0.5)
+		p2 = sum(mCountsParents>0.5)
+		vals = rbind(vals, c(s0, s1, s2, p0, p1, p2))
+	}
+	colnames(vals) = c("s0", "s1", "s2", "p0", "p1", "p2")
+	values(cnp) = vals
+
 	return(cnp)
 }
