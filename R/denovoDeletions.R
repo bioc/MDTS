200ccfa15ef9214bb355c96900fd6fb6b5773462
Jack Fu <jmfu@jhsph.edu>
2018-01-08 12:58:20 -0500

    findOverlaps IRanges

--- R/denovoDeletions.R
+++ R/denovoDeletions.R
@@ -22,7 +22,6 @@
 #' passing requite filters.
 denovoDeletions = function(cbs, mCounts, bins){
 	dels = GRanges()
-
 	print("Selecting Candidate de Novo deletions")
 	candidate = cbs[abs(cbs$m+1)<0.3]
 	candidate_fam_count = by(rep(1, length(candidate)), candidate$famid, sum)
@@ -33,19 +32,11 @@ denovoDeletions = function(cbs, mCounts, bins){
 	raw = abs(mCounts_local)<win
 	raw_perc = apply(raw, 1, mean)
 	raw_perc = sapply(raw_perc+1/dim(mCounts)[2], min, 1)
-
-	# ### New version
-	# ol = findOverlaps(candidate, bins)
-	# percs = by(subjectHits(ol), queryHits(ol), function(x) mean(raw_perc[x]))
-	# keep = which(percs>=0.95)
-	# dels = candidate[keep]
-
-	# ### Old version
 	bins_filter = bins[which(raw_perc<0.95)]
 
 	print("Filtering candidates by problematic bins")
-	ol_filter = findOverlaps(candidate, bins_filter)
-	ol_bins = findOverlaps(candidate, bins)
+	ol_filter = IRanges::findOverlaps(candidate, bins_filter)
+	ol_bins = IRanges::findOverlaps(candidate, bins)
 	if(length(ol_filter)>0){
 		count_filter = by(rep(1, length(ol_filter)), queryHits(ol_filter), sum)
 		count_bins = by(rep(1, length(ol_bins)), queryHits(ol_bins), sum)
