6580e7f58369b6d4f7d620b149e8ec8587e9309d
Jack Fu <jmfu@jhsph.edu>
2018-01-08 13:38:14 -0500

    new denovoDeletions + segmendMD

--- R/denovoDeletions.R
+++ R/denovoDeletions.R
@@ -3,8 +3,7 @@
 #' This function will return a single GRanges object containing all denovo 
 #' deletions that passed filtering from a Circular Binary Segmentation object 
 #' with supplementary information.
-#' @param cbs The GRanges output from segmentMD() that contains the copy number 
-#' segments of the families.
+#' @param cbs The output from segmentMD().
 #' @param mCounts The normalized counts matrix output by normalizeCounts().
 #' @param bins The set of bins determined by calcBins().
 #' @keywords denovoDeletions
@@ -23,20 +22,29 @@
 denovoDeletions = function(cbs, mCounts, bins){
 	dels = GRanges()
 	print("Selecting Candidate de Novo deletions")
-	candidate = cbs[abs(cbs$m+1)<0.3]
-	candidate_fam_count = by(rep(1, length(candidate)), candidate$famid, sum)
-	mCounts_local = mCounts
+	# candidate = cbs[abs(cbs$m+1)<0.3]
+	candidates = cbs[abs(cbs$seg.mean+1)<0.3,]
+	candidates$num.segs = candidates$end-candidates$start+1
 
 	print("Calculating problematic bins")
 	win = 0.5
-	raw = abs(mCounts_local)<win
+	raw = abs(mCounts)<win
 	raw_perc = apply(raw, 1, mean)
 	raw_perc = sapply(raw_perc+1/dim(mCounts)[2], min, 1)
-	bins_filter = bins[which(raw_perc<0.95)]
-
+	filter_ind = which(raw_perc<0.95)
+	
 	print("Filtering candidates by problematic bins")
-	ol_filter = IRanges::findOverlaps(candidate, bins_filter)
-	ol_bins = IRanges::findOverlaps(candidate, bins)
+	candidates$num.segs.filtered = sapply(1:dim(candidates)[1], .countBadBins, candidates, filter_ind)
+	
+	deletions = candidates[(candidates$num.segs.filtered/candidates$num.segs)<0.5]
+	deletions_gr = GRanges(seqnames=seqnames(bins)[deletions$start],
+                   IRanges(GenomicRanges::start(bins)[deletions$start],
+                           GenomicRanges::end(bins)[deletions$end]),
+                   m=deletions$seg.mean, famid=deletions$family)
+	return(deletions_gr)
+	# bins_filter = bins[filter_ind]
+	# ol_filter = IRanges::findOverlaps(candidate, bins_filter)
+	# ol_bins = IRanges::findOverlaps(candidate, bins)
 	# if(length(ol_filter)>0){
 	# 	count_filter = by(rep(1, length(ol_filter)), queryHits(ol_filter), sum)
 	# 	count_bins = by(rep(1, length(ol_bins)), queryHits(ol_bins), sum)
@@ -55,5 +63,10 @@ denovoDeletions = function(cbs, mCounts, bins){
 	# 	dels = candidate
 	# }
 	# return(dels)
-	return(candidate)
+}
+
+.countBadBins = function(i, candidates, filter_ind){
+      candidate = candidates[i,]
+      num.segs.bad = sum((filter_ind<=candidate$start & filter_ind>=candidate$start))
+      return(num.segs.bad)
 }
--- R/segmentMD.R
+++ R/segmentMD.R
@@ -19,17 +19,20 @@
 #'	md = calcMD(mCounts, bins, pD)
 #'	cbs = segmentMD(md, bins)
 #' @export
-#' @return A \code{GRanges} containing the segmented regions based on the input
+#' @return A \code{data.frame} containing the segmented regions based to be
+#' parsed by denovoDeletions()
 #' minimum distance.
 segmentMD = function(md, bins, alpha=0.001, undo.splits='sdundo', undo.SD=4, mc.cores=1){
-	family_segments = mclapply(1:(dim(md)[2]), .segmentMDCore, 
-            md=md, bins=bins, alpha=alpha, undo.splits = undo.splits, undo.SD=undo.SD, 
+	segs = mclapply(1:(dim(md)[2]), .segmentMDHelper, 
+            md=md, bins=bins, alpha=alpha, 
+            undo.splits = undo.splits, undo.SD=undo.SD, 
             mc.cores=mc.cores)
-	return(do.call(c, family_segments))
+	segs.out = do.call(rbind, segs)
+	return(segs.out)
 }
 
 ## Helper function
-.segmentMDCore = function(i, md, bins, alpha, undo.splits, undo.SD){
+.segmentMDHelper = function(i, md, bins, alpha, undo.splits, undo.SD){
       set.seed(137)
       message(paste0("Processing family number: ", i))
       md_sub = md[,i,drop=FALSE]
@@ -39,10 +42,14 @@ segmentMD = function(md, bins, alpha=0.001, undo.splits='sdundo', undo.SD=4, mc.
       cbs = segment(cna, alpha=alpha, undo.splits=undo.splits, undo.SD=undo.SD)
       
       segRows = cbs$segRows
-      gr = GRanges(seqnames=seqnames(bins)[segRows[,1]],
-                   IRanges(GenomicRanges::start(bins)[segRows[,1]], 
-                           GenomicRanges::end(bins)[segRows[,2]]),
-                   m=cbs$output$seg.mean,
-                   famid=colnames(md_sub))
-      return(gr)
+      out = data.frame(start=segRows[,1], end=segRows[,2], 
+                       seg.mean=cbs$output$seg.mean, family=colnames(md_sub))
+      return(out)
 }
+
+      # gr = GRanges(seqnames=seqnames(bins)[segRows[,1]],
+      #              IRanges(GenomicRanges::start(bins)[segRows[,1]], 
+      #                      GenomicRanges::end(bins)[segRows[,2]]),
+      #              m=cbs$output$seg.mean,
+      #              famid=colnames(md_sub))
+      # return(gr)
