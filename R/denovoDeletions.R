6c796c0dd90215cb21a91f81c0a4d8607877d370
Jack Fu <jmfu@jhsph.edu>
2018-03-07 14:44:22 -0500

    sapply -> vapply denovoDeletions.R

--- R/denovoDeletions.R
+++ R/denovoDeletions.R
@@ -29,12 +29,15 @@ denovoDeletions = function(cbs, mCounts, bins){
 	win <- 0.5
 	raw <- abs(mCounts)<win
 	raw_perc <- apply(raw, 1, mean)
-	raw_perc <- sapply(raw_perc+1/dim(mCounts)[2], min, 1)
+	raw_perc <- vapply(raw_perc+1/dim(mCounts)[2], min, double(1))
+	# raw_perc <- sapply(raw_perc+1/dim(mCounts)[2], min, 1)
 	filter_ind <- which(raw_perc<0.95)
 	
 	print("Filtering candidates by problematic bins")
-	candidates$num.segs.filtered <- sapply(1:dim(candidates)[1], 
-            .countBadBins, candidates, filter_ind)
+# 	candidates$num.segs.filtered <- sapply(1:dim(candidates)[1], 
+#             .countBadBins, candidates, filter_ind)
+	candidates$num.segs.filtered <- vapply(seq_along(dim(candidates)[1]), 
+            .countBadBins, FUN.VALUE=double(1), candidates, filter_ind)
 	
 	deletions <- candidates[(candidates$num.segs.filtered/
             candidates$num.segs)<0.5]
